241f7456e5b124d7fcca0aaef7b46c46c6140a27
Professional <av.sergey@gmail.com>
2025-10-06 21:51:34 +0300

    Вместо кнопки добавлено меню

--- cataas.py
+++ cataas.py
@@ -8,9 +8,9 @@ def load_image(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
-        image_data =BytesIO(response.content)
+        image_data = BytesIO(response.content)
         img = Image.open(image_data)
-        img.thumbnail((600,480), Image.Resampling.LANCZOS)
+        img.thumbnail((600, 480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as err:
         print(f'Произошла ошибка: {err}')
@@ -25,6 +25,10 @@ def set_image():
         label.image = img
 
 
+def exit():
+    window.destroy()
+
+
 window = Tk()
 window.title('Cats!')
 window.geometry('600x520')
@@ -32,8 +36,17 @@ window.geometry('600x520')
 label = Label()
 label.pack()
 
-update_button = Button(text='Обновить', command=set_image)
-update_button.pack()
+# update_button = Button(text='Обновить', command=set_image)
+# update_button.pack()
+
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label='Файл', menu=file_menu)
+file_menu.add_command(label='Загрузить фото', command=set_image)
+file_menu.add_separator()
+file_menu.add_command(label='Выход', command=exit)
 
 url = 'https://cataas.com/cat'
 
